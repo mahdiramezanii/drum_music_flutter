fe00b4ecba9dacf8f8eafd0d7ccca10c5bd7ec5b
mahdiramazani <mahdiramazani1281@gmail.com>
2024-01-27 17:36:14 +0330

    initial

---
+++ android/app/src/main/kotlin/com/example/drum_music_first/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drum_music_first
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetBody();
+  }
+}
+
+Widget GetBody() {
+  return MaterialApp(
+      home: Scaffold(
+    body: SafeArea(
+      child: DecoratedBox(
+          position: DecorationPosition.background,
+          decoration: BoxDecoration(
+              color: Colors.yellow,
+              image: DecorationImage(image: AssetImage("assets/drum.png"))),
+          child: Center(
+            child: Center(
+              child: ClickScrean(),
+            ),
+          )),
+    ),
+  ));
+}
+
+Widget ClickScrean() {
+  return Column(
+    children: [
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('assets_h1.wav'));
+                    },
+                    child: Text(""))),
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('assets_k1.wav'));
+                    },
+                    child: Text(""))),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('assets_k2.wav'));
+                    },
+                    child: Text(""))),
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('assets_c1.wav'));
+                    },
+                    child: Text(""))),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('assets_c2.wav'));
+                    },
+                    child: Text(""))),
+            Expanded(child: TextButton(onPressed: () {}, child: Text(""))),
+          ],
+        ),
+      ),
+    ],
+  );
+}
